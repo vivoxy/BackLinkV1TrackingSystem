8adaf800f788cc24c2ec450bf6ead1fc62c327cf
Northeaster <35185688+vivoxy@users.noreply.github.com>
2018-01-12 15:13:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-	include "sistem/sistem.php";
-	if($_SESSION["oturum"]==true){ 
-	include "sayfalar/ust.php";
-	include "sayfalar/sol.php";
-	include "sayfalar/$CekOguzDosyayi.php";
-	include "sayfalar/alt.php";
-	}if($_SESSION['hesapadi']=="" OR $_SESSION["oturum"]==false){ 
-	include "sayfalar/giris.php";}
-
-?>
+edit
